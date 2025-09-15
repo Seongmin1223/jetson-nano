0ddf393f5a8fe6c1ef3fb7c4581135a86c5ad7e6
ChoiYoonho <dbsgh725@naver.com>
2025-09-15 18:17:01 +0900

    오류 수정

--- 최윤호/GrayMode.py
+++ 최윤호/GrayMode.py
@@ -1,10 +1,11 @@
-
+import cv2
 def GreyMode(frame):
-    b=frame[:,:,0]
-    g=frame[:,:,1]
-    r=frame[:,:,2]
-    grayFrame=(r*0.299)+(g*0.587)+(b*0.114)
-    grayFrame=grayFrame.astype('uint8')
-
-    return grayFrame
+    b = frame[:, :, 0]
+    g = frame[:, :, 1]
+    r = frame[:, :, 2]
+    grayFrame = (r*0.299) + (g*0.587) + (b*0.114)
+    grayFrame = grayFrame.astype('uint8')
     
+    # 단일 채널 → 3채널 변환
+    grayFrame = cv2.cvtColor(grayFrame, cv2.COLOR_GRAY2BGR)
+    return grayFrame
